b5e2d0dfa47a7344ae2312f7c445d949c8e78c94
TomasAbkiewicz <166508961+TomasAbkiewicz@users.noreply.github.com>
2025-08-04 22:12:08 -0300

    gweg

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject optionsPanel;
+    public Slider volumeSlider;
+    public Toggle musicToggle;
+    public Slider brightnessSlider;
+    public AudioSource musicSource;
+    public GameObject brightnessOverlay;
+
+    void Start()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(Enemy.Demo);
+    }
+
+    public void OpenOptions()
+    {
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    public void SetVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+
+    public void ToggleMusic(bool isOn)
+    {
+        if (musicSource != null)
+            musicSource.mute = !isOn;
+    }
+
+    public void SetBrightness(float value)
+    {
+        if (brightnessOverlay != null)
+        {
+            var color = brightnessOverlay.GetComponent<Image>().color;
+            color.a = 1f - value;
+            brightnessOverlay.GetComponent<Image>().color = color;
+        }
+    }
+}
