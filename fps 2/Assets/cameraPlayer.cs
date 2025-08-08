10469877cbdfd1d98f2e6cfba5528ed41e179fbf
TomasAbkiewicz <166508961+TomasAbkiewicz@users.noreply.github.com>
2025-08-07 21:34:11 -0300

    fps 2 sumado a git

---
+++ fps 2/Assets/cameraPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraPlayer : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // input del mouse 
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensX * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensY * Time.deltaTime;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        // máximo de rotación 
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotar la cámara y la orientación
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
+
