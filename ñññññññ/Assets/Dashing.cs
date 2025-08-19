8f38ee5363582a0d1f0c986332680aacf75dde7e
TomasAbkiewicz <166508961+TomasAbkiewicz@users.noreply.github.com>
2025-08-18 22:27:02 -0300

    ñññ

---
+++ ñññññññ/Assets/Dashing.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dashing : MonoBehaviour
+{
+    [Header("Reference")]
+    public Transform orientation;
+    public Transform playerCam; 
+    private Rigidbody rb;
+    private PlayerMovement pm;
+
+    [Header("Dash Settings")]
+    public float dashForce;
+    public float dashUpwardForce;
+    public float dashDuration;
+
+    [Header("Cooldown")]
+    public float dashCd;
+    private float dashCdTimer;
+
+    [Header("Input")]  
+    public KeyCode dashKey = KeyCode.E;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(dashKey))
+            Dash();
+    }
+
+    private void Dash()
+    {
+        Vector3 forceToApply = orientation.forward * dashForce + orientation.up * dashUpwardForce;
+
+        rb.AddForce(forceToApply, ForceMode.Impulse); 
+
+        Invoke(nameof(ResetDash), dashDuration);
+    }
+
+    private void ResetDash()
+    {
+       
+    }
+}
---
+++ ñññññññ/Assets/PlayerCam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+
+    public Transform orientation;   
+
+    float xRotation;
+    float yRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensX * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensY * Time.deltaTime;
+        // rotate camera
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        // clamp vertical rotationz
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        // apply rotations
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
